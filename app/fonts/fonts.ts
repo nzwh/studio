120af9e72ce01cd29700367c9a51724303fa259c
nzwh <notzerowh@gmail.com>
2026-04-21 15:49:55 +0800

    chore: add cmu-concrete font

--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -20,3 +20,14 @@ export const ppnikkeimaru = localFont({
   variable: '--font-pp-nikkei',
   display: 'swap'
 })
+
+export const cmuconcrete = localFont({
+  src: [
+    { path: 'cmu-concrete/cmuconcrete-nobi.ttf', weight: '800', style: 'italic' },
+    { path: 'cmu-concrete/cmuconcrete-nobx.ttf', weight: '800', style: 'normal' },
+    { path: 'cmu-concrete/cmuconcrete-noti.ttf', weight: '400', style: 'italic' },
+    { path: 'cmu-concrete/cmuconcrete-norm.ttf', weight: '400', style: 'normal' }
+  ],
+  variable: '--font-cmu-concrete',
+  display: 'swap'
+})
