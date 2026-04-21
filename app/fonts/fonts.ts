cf05eda750948f55120d562505f009a293370ff3
nzwh <notzerowh@gmail.com>
2026-04-21 20:27:23 +0800

    chore: add redaction font

--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -23,11 +23,25 @@ export const ppnikkeimaru = localFont({
 
 export const cmuconcrete = localFont({
   src: [
-    { path: 'cmu-concrete/cmuconcrete-nobi.ttf', weight: '800', style: 'italic' },
-    { path: 'cmu-concrete/cmuconcrete-nobx.ttf', weight: '800', style: 'normal' },
-    { path: 'cmu-concrete/cmuconcrete-noti.ttf', weight: '400', style: 'italic' },
-    { path: 'cmu-concrete/cmuconcrete-norm.ttf', weight: '400', style: 'normal' }
+    { path: 'cmu-concrete/cmuconcrete-nobi.ttf',  weight: '800', style: 'italic' },
+    { path: 'cmu-concrete/cmuconcrete-nobx.ttf',  weight: '800', style: 'normal' },
+    { path: 'cmu-concrete/cmuconcrete-noti.ttf',  weight: '400', style: 'italic' },
+    { path: 'cmu-concrete/cmuconcrete-norm.ttf',  weight: '400', style: 'normal' }
   ],
   variable: '--font-cmu-concrete',
+  display: 'swap'
+})
+
+
+export const redaction = localFont({
+  src: [
+    { path: 'redaction/redaction20-regular.otf',  weight: '400', style: 'normal' },
+    { path: 'redaction/redaction20-italic.otf',   weight: '400', style: 'italic' },
+    { path: 'redaction/redaction20-bold.otf',     weight: '500', style: 'normal' },
+    { path: 'redaction/redaction35-regular.otf',  weight: '600', style: 'normal' },
+    { path: 'redaction/redaction35-italic.otf',   weight: '600', style: 'italic' },
+    { path: 'redaction/redaction35-bold.otf',     weight: '700', style: 'normal' }
+  ],
+  variable: '--font-redaction',
   display: 'swap'
 })
