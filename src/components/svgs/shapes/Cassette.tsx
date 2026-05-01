e454c39542c1b711bf90e44d259a31aed314b5f0
nzwh <notzerowh@gmail.com>
2026-05-01 18:48:22 +0800

    fix: svg stroke corner smoothing

--- src/components/svgs/shapes/Cassette.tsx
+++ src/components/svgs/shapes/Cassette.tsx
@@ -3,7 +3,7 @@ interface CassetteShapeProps {
   fill?: string;
   stroke?: string;
 }
-export default function Cassette({
+export default function CassetteShape({
   className,
   fill = "transparent",
   stroke = "currentColor",
@@ -18,6 +18,9 @@ export default function Cassette({
       <g filter="url(#filter0_i_149_1284)">
         <path
           d="M0 8C0 3.58172 3.58172 0 8 0H200.059C206.424 0 212.529 2.52856 217.029 7.02944L222.971 12.9706C227.471 17.4714 233.576 20 239.941 20H427.079C432.223 20 437.231 18.3472 441.365 15.2854L455.635 4.71463C459.769 1.65276 464.777 0 469.921 0H664C668.418 0 672 3.58172 672 8V45C672 47.7614 669.761 50 667 50C664.239 50 662 52.2386 662 55C662 57.7614 664.239 60 667 60C669.761 60 672 62.2386 672 65V256C672 260.418 668.418 264 664 264H8.00001C3.58173 264 0 260.418 0 256V65C0 62.2386 2.23858 60 5 60C7.76142 60 10 57.7614 10 55C10 52.2386 7.76142 50 5 50C2.23858 50 0 47.7614 0 45V8Z"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          paintOrder="stroke fill"
           fill={fill}
           stroke={stroke}
         />
@@ -25,10 +28,10 @@ export default function Cassette({
       <defs>
         <filter
           id="filter0_i_149_1284"
-          x="0"
-          y="-20"
-          width="672"
-          height="284"
+          x="-1"
+          y="-21"
+          width="674"
+          height="286"
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
--- src/components/svgs/shapes/Monitor.tsx
+++ src/components/svgs/shapes/Monitor.tsx
@@ -3,7 +3,7 @@ interface MonitorShapeProps {
   fill?: string;
   stroke?: string;
 }
-export default function Monitor({
+export default function MonitorShape({
   className,
   fill = "transparent",
   stroke = "currentColor",
@@ -19,6 +19,9 @@ export default function Monitor({
         d="M0 8C0 3.58172 3.58172 0 8 0H168H265C269.418 0 273 3.58172 273 8V12C273 16.4183 276.582 20 281 20H391C395.418 20 399 16.4183 399 12V8C399 3.58172 402.582 0 407 0H504H664C668.418 0 672 3.58172 672 8V352C672 356.418 668.418 360 664 360H8.00001C3.58173 360 0 356.418 0 352V8Z"
         fill={fill}
         stroke={stroke}
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        paintOrder="stroke fill"
       />
     </svg>
   );
